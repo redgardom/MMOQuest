e37d5f5884e9835814ced641c824d96e766dbb05
dominique <domlachance-forget@hotmail.com>
2018-05-26 02:53:41 -0400

    test

---
+++ src/main/java/com/redgardom/MMOQuests/MMOQuests.java
@@ -0,0 +1,32 @@
+package com.redgardom.MMOQuests;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="MMOQuests", name="MMO Quests", version="1.7.10-1.0")
+public class MMOQuests 
+{
+	@Mod.Instance("MMOQuests")
+	public static MMOQuests instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent  event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent  event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent  event)
+	{
+		
+	}
+
+}
